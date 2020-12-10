86bd2a859ee025da02f3259d762a16302b5ab4b6
Jonathan Yeh <jmyeh51@gmail.com>
2020-12-10 12:17:03 -0800

    finished logic

--- src/NFA.spec.ts
+++ src/NFA.spec.ts
@@ -9,68 +9,113 @@ const machineTests: {
         rejected: string[],
     }
 } = {
-    startsWith0: {
+    accept_10_or_01: {
+        // Add your accepted strings here
         accepted: [
-            '0',
-            '01',
-            '000000',
-            '00001111111',
-        ],
-        rejected: [
             '10',
-            '',
+            '01',
+        ],
+        // Add any rejected strings you want checked here
+        rejected: [
             '101',
             '1000000',
             '101111',
         ],
+        // Build your NFA here; for a lambda move, use "lambda"
         description: {
             transitions: {
-                r0: {
-                    0: 'r1',
-                    1: 'r2',
+                S: {
+                    lambda: ['A', 'B']
                 },
-                r1: {
-                    0: 'r1',
-                    1: 'r1',
+                A: {
+                    1: 'C',
                 },
-                r2: {
-                    0: 'r2',
-                    1: 'r2',
+                C: {
+                    0: 'D',
+                },
+                B: {
+                    0: 'E',
+                },
+                E: {
+                    1: 'F',
                 },
             },
-            start: 'r0',
-            acceptStates: ['r1'],
+            acceptStates: ['D', 'F'],
+            statesWithLambda: ['S'],
         }
-    }
+    },
+    acceptRegex: {
+        // Add your accepted strings here
+        accepted: [
+            '1101010101010',
+            ' ',
+            '11111111111111110',
+            '100000000',
+            '10'
+        ],
+        // Add any rejected strings you want checked here
+        rejected: [
+            '1',
+            '0',
+            '11',
+            '10000000000000000000000001',
+            '000000010'
+        ],
+        // Build your NFA here; for a lambda move, use "lambda"
+        description: {
+            transitions: {
+                S: {
+                    lambda: ['A', 'B']
+                },
+                A: {
+                    1: 'C',
+                },
+                C: {
+                    0: ['C', 'D'],
+                    1: 'C',
+                },
+            },
+            acceptStates: ['B', 'D'],
+            statesWithLambda: ['S'],
+        }
+    },
 }
 
 for (const [name, testDescription] of Object.entries(machineTests)) {
-    test(`${name}/constructor`, (t) => {
-        const nfa = new NFA(testDescription.description);
-        t.truthy(nfa);
-    });
+    // test(`${name}/constructor`, (t) => {
+    //     const nfa = new NFA(testDescription.description);
+    //     t.truthy(nfa);
+    // });
 
-    test(`${name}/transition`, (t) => {
-        const nfa = new NFA(testDescription.description);
-        const { transitions } = testDescription.description;
+    // test(`${name}/transition`, (t) => {
+    //     const nfa = new NFA(testDescription.description);
+    //     const { transitions } = testDescription.description;
 
-        for (const [state, stateTransition] of Object.entries(transitions)) {
-            for (const [symbol, nextState] of Object.entries(stateTransition)) {
-                t.assert(nextState === nfa.transition(state, symbol));
-            }
-        }
-    });
+    //     for (const [state, stateTransition] of Object.entries(transitions)) {
+    //         for (const [symbol, nextState] of Object.entries(stateTransition)) {
+    //             t.assert(nextState === nfa.getTransitions(state, symbol));
+    //         }
+    //     }
+    // });
 
     test(`${name}/accept`, (t) => {
         const nfa = new NFA(testDescription.description);
         const { accepted, rejected } = testDescription;
 
         for (const s of accepted) {
-            t.assert(nfa.accepts(s));
+            console.log("Testing String: " + s);
+            console.log("-------------------------");
+            t.assert(nfa.accepts(s, 'S'));
+            console.log("   PASS    ");
+            console.log(" ");
         }
 
         for (const s of rejected) {
-            t.assert(!nfa.accepts(s));
+            console.log("Testing String: " + s);
+            console.log("-------------------------");
+            t.assert(!nfa.accepts(s, 'S'));
+            console.log("   PASS    ");
+            console.log(" ");
         }
     });
 }
--- src/NFA.ts
+++ src/NFA.ts
@@ -1,18 +1,16 @@
 
 
-type State = string;
 type InputSymbol = string;
 
 
 export interface NFADescription {
     transitions: {
         [key:string]: {
-            0: State,
-            1: State,
+
         }
     },
-    start: State,
-    acceptStates: State[],
+    acceptStates: string[],
+    statesWithLambda: string[],
 }
 
 export default class NFA {
@@ -22,20 +20,64 @@ export default class NFA {
         this.description = description;
     }
 
-    transition(state: State, symbol: InputSymbol): State {
+    getTransitions(state, symbol: InputSymbol): string[] {
         const { description: { transitions } } = this;
-        return transitions[state][symbol];
+        if (transitions[state] == undefined) {
+            return ['-1'];
+        }
+        let transition = transitions[state][symbol];
+        if (transition == undefined) {
+            return ['-1'];
+        }
+        return transition;
     }
 
-    accepts(s: string): boolean {
-        const { description: { start, acceptStates } } = this;
+    handleLambda(s:string, state:string): boolean {
+        console.log("lambda string: " + s + " lambda state: " + state);
+        const transitions = this.getTransitions(state, 'lambda');
+        console.log("Lambda transitions: " + transitions);
 
-        let state = start;
-
-        for (const symbol of s) {
-            state = this.transition(state, symbol);
+        for (let element in transitions) {
+            console.log("lambda string: " + s + " lambda element: " + transitions[element]);
+            if (this.accepts(s, transitions[element])) {
+                return true;
+            }
         }
 
-        return acceptStates.includes(state);
+        return false;
+    }
+
+    accepts(s: string, state: string): boolean {
+        s = s.trim();
+        console.log("this.accept string: " + s);
+        console.log("this.accept state: " + state);
+        const { description: { acceptStates, statesWithLambda } } = this;
+
+        // Base case
+        if(s.length == 0 && acceptStates.includes(state)) {
+            return true;
+        }
+
+        console.log("this.accept state: " + state + " s.charAt(0): " + s.charAt(0));
+        const nextStates = this.getTransitions(state, s.charAt(0));
+        console.log("nextStates: " + nextStates);
+
+        if (!nextStates.includes('-1')) {
+            // handle regular transitions
+            for (let element in nextStates) {
+                if (this.accepts(s.substr(1), nextStates[element])) {
+                    return true;
+                }
+            }
+        }
+
+        // check for lambda and handle lambda
+        if (statesWithLambda.includes(state)) {
+            console.log("Handling Lambda: ---------------");
+            return this.handleLambda(s, state);
+        }
+
+
+        return false;
     }
 }
